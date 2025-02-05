3ebced02793fcaf9fa2a5b588fb81e989efc83db
Abhay2003-ops <abhaymehra220@gmail.com>
2025-02-05 16:47:16 +0530

    task3

---
+++ variables_demo.py
@@ -0,0 +1,6 @@
+student_name = "Abhay"
+student_age = 22
+student_roll_no = 1
+student_marks = 90.5
+
+print(f"student: {student_name}, age: {student_age}, roll no: {student_roll_no}, marks: {student_marks}")
