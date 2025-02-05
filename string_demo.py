1784bb8bfbde5b7e25897be220159fc14817a1f9
Abhay2003-ops <abhaymehra220@gmail.com>
2025-02-05 16:48:47 +0530

    task4

---
+++ string_demo.py
@@ -0,0 +1,12 @@
+# String Manipulation
+sentence = "Python is awesome!"
+
+# Uppercase
+print(sentence.upper())
+
+# Replace words
+new_sentence = sentence.replace("awesome", "powerful")
+print(new_sentence)
+
+# Check if string contains a word
+print("Python" in sentence)  # True
